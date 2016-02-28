2bc385d691451f61cd293e1cf2d7e5fffd5a2876
Sangwon Lee <sangwon.lee@rgpkorea.co.kr>
2016-02-28 19:25:00 +0900

    issue#22: pull request to owner repo

--- setup.py
+++ setup.py
@@ -16,13 +16,13 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.9',
+    version='0.2.2',
 
     description='CLI for the happy coding process',
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/caststack/cykle',
+    url='https://github.com/sangwonl/cykle',
 
     # Author details
     author='Sangwon Lee',
